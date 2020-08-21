f62ef9894b4610678d8895a8343e2c96ce3c20ae
manvijain08 <jainmanvi017@gmail.com>
2020-08-21 16:02:19 +0530

    Updates

---
+++ precache-manifest.bdf946918e1639cd748cdf34da0a8445.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f569e73713d33d3d0a86ba80d32c1833",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "ec304aefede0dffb0970",
+    "url": "/todo-react/static/css/main.9cb5d323.chunk.css"
+  },
+  {
+    "revision": "05b6083655ece5a72a79",
+    "url": "/todo-react/static/js/2.585c3714.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.585c3714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec304aefede0dffb0970",
+    "url": "/todo-react/static/js/main.d8866c45.chunk.js"
+  },
+  {
+    "revision": "a049eaa3d15bfcc71b2e",
+    "url": "/todo-react/static/js/runtime-main.b204df10.js"
+  }
+]);
