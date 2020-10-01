ed331a580e691eccf82c5b5026f6d944f832c8c5
Priyansh-jsk <58244081+Priyansh-jsk@users.noreply.github.com>
2020-10-01 10:00:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react";               #importing react 
+import ReactDOM from "react-dom";       #Syntax of ReactDOM reader(element, container[callback])
 import App from "./App";
 ReactDOM.render(
   <React.StrictMode>
